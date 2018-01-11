6c0134e8b98622cfbbb798ff5f8d54f623748d9e
laradashut <lara.dashut@gmail.com>
2018-01-11 13:36:53 -0800

    server

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+// // const router = express.Router();
+// const axios = require('axios');
+
+// const router = require('./routes.js');
+// const db = require('../database');
+
+const app = express();
+
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use('/events', (req, res) => {
+
+// });
+// app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
+
+module.exports.app = app;
